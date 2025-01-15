e01ea9e02d6f73fb50283c6cacdac8f280b00dd2
jmvdr-iscte <ze.correia467@gmail.com>
2025-01-15 18:05:22 +0000

    Change config

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,9 +5,12 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 php-fpm -D
 
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+cd /app
 
-php /app/artisan migrate --force
+# Run Laravel migrations
+php artisan migrate --force
 
-php /app/artisan schedule:work &
+# Start Laravel Scheduler in the background
+php artisan schedule:work &
 
 nginx
